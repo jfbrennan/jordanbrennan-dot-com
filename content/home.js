128daf9dac60034ad951d3137e8b2270846449ee
Jordan Brennan <jobrennan@egencia.com>
2014-10-08 19:32:36 -0700

    Updates content (still pending final content).

--- content/home.js
+++ content/home.js
@@ -7,105 +7,153 @@ exports.header = {
 
 exports.body = {
     hardware: {
+        video: {
+            id: "iJ4T9CQA0UM",
+            text: "Lorem ipsum dolor wathc the video",
+            runtime: "5:23"
+        },
         mainCopy: {
             subtitle: "Hardware",
             title: "All the Right I/O for Professionals",
             text: "Bacon ipsum dolor sit amet ribeye pork belly sausage shankle salami, landjaeger pork chop brisket kielbasa pork loin meatball prosciutto hamburger shank. Pancetta venison drumstick tail cow meatball pork belly. Pastrami beef ribs filet mignon short ribs meatball kielbasa. Frankfurter leberkas turducken fatback, short ribs ham hock strip steak doner short loin pork shank. "
         },
-        features: {
-            title: "Collaborate",
-            text: "Multiple engineers can work from a single mixer."
+        features:  {
+            items: [
+                {
+                    imgUrl: "http://placehold.it/286x190",
+                    count: "32",
+                    text: "All-new Onyx+&trade; recallable mic press w/ wireless control"
+                },
+                {
+                    imgUrl: "http://placehold.it/280x190",
+                    count: "14",
+                    text: "Fully-assignable XLR outputs"
+                }
+            ]
         }
     },
     wireless: {
+        video: {
+            id: "iJ4T9CQA0UM",
+            text: "Lorem ipsum dolor wathc the video!",
+            runtime: "5:23"
+        },
         mainCopy: {
             subtitle: "Wireless",
             title: "Control Everything From Anywhere",
             text: "Bacon ipsum dolor sit amet ribeye pork belly sausage shankle salami, landjaeger pork chop brisket kielbasa pork loin meatball prosciutto hamburger shank. Pancetta venison drumstick tail cow meatball pork belly. Pastrami beef ribs filet mignon short ribs meatball kielbasa. Frankfurter leberkas turducken fatback, short ribs ham hock strip steak doner short loin pork shank. "
         },
-        features: [
-            {
-                title: "Collaborate",
-                text: "Multiple engineers can work from a single mixer."
-            },
-            {
-                title: "Collaborate",
-                text: "Multiple engineers can work from a single mixer."
-            },
-            {
-                title: "Collaborate",
-                text: "Multiple engineers can work from a single mixer."
-            }
-        ]
+        features: {
+            items: [
+                {
+                    title: "Collaborate",
+                    text: "Multiple engineers can work from a single mixer."
+                },
+                {
+                    title: "Collaborate",
+                    text: "Multiple engineers can work from a single mixer."
+                }
+            ]
+        }
     },
     recordPlay: {
+        video: {
+            id: "iJ4T9CQA0UM",
+            text: "Lorem ipsum dolor wathc the video!",
+            runtime: "5:23"
+        },
         mainCopy: {
             subtitle: "Record/Play",
             title: "Control Everything From Anywhere",
             text: "Bacon ipsum dolor sit amet ribeye pork belly sausage shankle salami, landjaeger pork chop brisket kielbasa pork loin meatball prosciutto hamburger shank. Pancetta venison drumstick tail cow meatball pork belly. Pastrami beef ribs filet mignon short ribs meatball kielbasa. Frankfurter leberkas turducken fatback, short ribs ham hock strip steak doner short loin pork shank. "
         },
-        features: [
-            {
-                title: "Collaborate",
-                text: "Multiple engineers can work from a single mixer."
-            },
-            {
-                title: "Collaborate",
-                text: "Multiple engineers can work from a single mixer."
-            }
-        ]
+        features: {
+            items: [
+                {
+                    title: "Collaborate",
+                    text: "Multiple engineers can work from a single mixer."
+                },
+                {
+                    title: "Collaborate",
+                    text: "Multiple engineers can work from a single mixer."
+                }
+            ]
+        }
     },
     dsp: {
+        video: {
+            id: "iJ4T9CQA0UM",
+            text: "Lorem ipsum dolor wathc the video!",
+            runtime: "5:23"
+        },
         mainCopy: {
             subtitle: "Digital Sound Processing (DSP)",
             title: "Control Everything From Anywhere",
             text: "Bacon ipsum dolor sit amet ribeye pork belly sausage shankle salami, landjaeger pork chop brisket kielbasa pork loin meatball prosciutto hamburger shank. Pancetta venison drumstick tail cow meatball pork belly. Pastrami beef ribs filet mignon short ribs meatball kielbasa. Frankfurter leberkas turducken fatback, short ribs ham hock strip steak doner short loin pork shank. "
         },
-        features: [
-            {
-                count: "36",
-                title: "Collaborate",
-                text: "Multiple engineers can work from a single mixer."
-            },
-            {
-                count: "28",
-                title: "Collaborate",
-                text: "Multiple engineers can work from a single mixer."
-            },
-            {
-                count: "03",
-                title: "Collaborate",
-                text: "Multiple engineers can work from a single mixer."
-            },
-            {
-                footnote: true,
-                text: "Return channels and subgroups and what not and compression."
-            }
-        ]
+        features: {
+            withCount: true,
+            items: [
+                {
+                    count: "36",
+                    title: "Collaborate",
+                    text: "Multiple engineers can work from a single mixer."
+                },
+                {
+                    count: "28",
+                    title: "Collaborate",
+                    text: "Multiple engineers can work from a single mixer."
+                },
+                {
+                    count: "03",
+                    title: "Collaborate",
+                    text: "Multiple engineers can work from a single mixer."
+                },
+                {
+                    footnote: "*",
+                    text: "Return channels and subgroups and what not and compression."
+                }
+            ]
+        }
     },
     masterFader: {
+        video: {
+            id: "iJ4T9CQA0UM",
+            text: "Lorem ipsum dolor wathc the video!",
+            runtime: "5:23"
+        },
         mainCopy: {
             subtitle: "Master Fader",
             title: "Control Everything From Anywhere",
             text: "Bacon ipsum dolor sit amet ribeye pork belly sausage shankle salami, landjaeger pork chop brisket kielbasa pork loin meatball prosciutto hamburger shank. Pancetta venison drumstick tail cow meatball pork belly. Pastrami beef ribs filet mignon short ribs meatball kielbasa. Frankfurter leberkas turducken fatback, short ribs ham hock strip steak doner short loin pork shank. "
         },
-        features: [
-            {
-                title: "Collaborate",
-                text: "Multiple engineers can work from a single mixer."
-            },
-            {
-                title: "Collaborate",
-                text: "Multiple engineers can work from a single mixer."
-            },
-            {
-                title: "Collaborate",
-                text: "Multiple engineers can work from a single mixer."
-            },
-            {
-                title: "Collaborate",
-                text: "Multiple engineers can work from a single mixer."
-            }
-        ]
+        features: {
+            items: [
+                {
+                    title: "Collaborate",
+                    text: "Multiple engineers can work from a single mixer."
+                },
+                {
+                    title: "Collaborate",
+                    text: "Multiple engineers can work from a single mixer."
+                },
+                {
+                    title: "Collaborate",
+                    text: "Multiple engineers can work from a single mixer."
+                },
+                {
+                    title: "Collaborate",
+                    text: "Multiple engineers can work from a single mixer."
+                }
+            ]
+        }
+    }
+};
+
+exports.footer = {
+    video: {
+        id: "iJ4T9CQA0UM",
+        text: "Lorem ipsum dolor wathc the video!",
+        runtime: "5:23"
     }
 };
